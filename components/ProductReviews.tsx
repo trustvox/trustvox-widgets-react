dcdb86b12aefb572781a67c7bc362aa82e7f7c50
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-08-01 00:40:38 -0400

    Implement Trustvox widget

--- components/ProductReviews.tsx
+++ components/ProductReviews.tsx
@@ -1,16 +1,83 @@
+import React from 'react';
 
-import PropTypes from 'prop-types';
+interface ReviewsProps {
+  product: any;
+}
 
-const ProductReviews = ({product}) => {
-  return (
-    <div>
-      <div id="_sincero_widget"></div>
-    </div>
-  )
-};
+class ProductReviews extends React.Component<ReviewsProps> {
+  private setTrustvoxAttrs() {
+    window['_trustvox'] = [];
 
-ProductReviews.propTypes = {
-  product: PropTypes.object.isRequired
-};
+    window['_trustvox'].push(
+      ['_storeId', this.props.product.storeId],
+      ['_productId', this.props.product.productId],
+      ['_customStyle', this.props.product.customStyle || {}]
+    );
+
+    if (this.props.product.name)
+      window['_trustvox'].push(['_productName', this.props.product.name]);
+
+    if (this.props.product.photos)
+      window['_trustvox'].push(['_productPhotos', this.props.product.photos]);
+  }
+
+  private addTrustvoxScript() {
+    if (!this.isTrustvoxScriptLoaded()) {
+      const script = window.document.createElement('script');
+      script.src =
+        '//static-staging.trustvox.com.br/sincero-staging/sincero.js';
+      script.async = false;
+      script.defer = true;
+      document.head.appendChild(script);
+    }
+  }
+
+  private autoloadTrustvox() {
+    if (window['_trustvox_initializer'])
+      window['_trustvox_initializer'].autoInitialize();
+  }
+
+  private cleanupTrustvox() {
+    if (window['_trustvox_initializer'])
+      window['_trustvox_initializer'].cleanup();
+  }
+
+  private isTrustvoxScriptLoaded() {
+    return !!document.querySelector(
+      "[src='//static-staging.trustvox.com.br/sincero-staging/sincero.js']"
+    );
+  }
+
+  // Only necessary for this demo. Please, don't put this in production
+  private cleanupStyles() {
+    const customStyle = document.querySelector('style[data-jss]');
+    if (customStyle) document.head.removeChild(customStyle);
+  }
+
+  private initializeTrustvox() {
+    this.setTrustvoxAttrs();
+    this.addTrustvoxScript();
+    this.cleanupTrustvox();
+    this.autoloadTrustvox();
+  }
+
+  public componentDidMount() {
+    this.cleanupStyles();
+    this.initializeTrustvox();
+  }
+
+  public componentDidUpdate() {
+    this.cleanupStyles();
+    this.initializeTrustvox();
+  }
+
+  public render() {
+    return (
+      <div>
+        <div id="_sincero_widget"></div>
+      </div>
+    );
+  }
+}
 
 export default ProductReviews;
