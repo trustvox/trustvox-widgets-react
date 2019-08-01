1a5c4a431f501df7421a8b20f326b78594d04666
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-08-01 00:10:11 -0400

    Implement product page rendering

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,11 +1,27 @@
+import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
 
-const Home = () => (
+import { getProduct } from '../utils/products';
+
+const loadingPage = (
   <Layout>
-    <div v-if="product">
-      <h2 className="title is-2">Product 1</h2>
-    </div>
+    <div>Loading...</div>
   </Layout>
 );
+
+const Home = () => {
+  const router = useRouter();
+  const product = getProduct(router.query.id);
+
+  if (!product) return loadingPage;
+
+  return (
+    <Layout>
+      <div v-if="product">
+        <h2 className="title is-2">{product.title}</h2>
+      </div>
+    </Layout>
+  );
+};
 
 export default Home;
