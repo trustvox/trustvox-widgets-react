dd71a7e8d7301e1631813ce0f05dfe924532894c
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-07-31 23:37:42 -0400

    Add about page

---
+++ pages/about.tsx
@@ -0,0 +1,22 @@
+
+import Layout from '../components/Layout';
+
+const About = () => (
+  <Layout>
+    <div>
+      <h2 className="title is-h2">Sobre</h2>
+
+      <p>
+        Esta é uma demonstração dos Widgets da RA Trustvox executadas dentro de
+        uma aplicação React.
+      </p>
+
+      <p>
+        Todos os dados listados aqui são de homologação e não representa nenhuma
+        situação do mundo real.
+      </p>
+    </div>
+  </Layout>
+);
+
+export default About;
